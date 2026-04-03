1c9e34dad8bea463fcdf73c6c617a3cf2aae4c26
D.chandu naik <139032331+chandu1267@users.noreply.github.com>
2026-04-03 12:28:12 +0530

    updated API base URL

--- src/repo/api_path.js
+++ src/repo/api_path.js
@@ -1,6 +1,14 @@
 
-export const productUrl = "http://13.235.76.33:3000/product"
-export const imageUrl = "http://13.235.76.33:3000"
-export const adminUrl = "http://13.235.76.33:3000/admin"
-export const emailUrl = "http://13.235.76.33:3000/email"
-export const cartUrl = "http://13.235.76.33:3000/cart"
+export const BASE_URL = "https://farm2cart-backend.onrender.com";
+
+export const productUrl = `${BASE_URL}/product`;
+export const imageUrl = BASE_URL;
+export const adminUrl = `${BASE_URL}/admin`;
+export const emailUrl = `${BASE_URL}/email`;
+export const cartUrl = `${BASE_URL}/cart`;
+
+// export const productUrl = "http://13.235.76.33:3000/product"
+// export const imageUrl = "http://13.235.76.33:3000"
+// export const adminUrl = "http://13.235.76.33:3000/admin"
+// export const emailUrl = "http://13.235.76.33:3000/email"
+// export const cartUrl = "http://13.235.76.33:3000/cart"
